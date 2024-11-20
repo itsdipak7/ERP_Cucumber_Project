ff46b1622dd7907be1b43309568015f9b7b56ced
DELL <DELL@DipakKumarGouda>
2024-11-20 12:05:24 -0800

    First Commit

---
+++ Cucumber_ERP/src/test/java/testRunner/AppTest.java
@@ -0,0 +1,14 @@
+package testRunner;
+
+import org.junit.runner.RunWith;
+
+import cucumber.api.CucumberOptions;
+import cucumber.api.junit.Cucumber;
+import cucumber.api.testng.AbstractTestNGCucumberTests;
+
+//@RunWith(Cucumber.class)
+@CucumberOptions(features = {"FeatureFiles"},monochrome = true,glue = {"stepDefination"}
+,plugin= {"pretty","html:target/report/cucumber","json:target/report/cucumber.json","junit:target/report/cucumber.xml"})
+public class AppTest extends AbstractTestNGCucumberTests{
+
+}
